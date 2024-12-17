0aa19da0fa74535ba4ca54b5c62260a64adb1d56
caarlosdlg <caarllossdlg@gmail.com>
2024-12-16 22:37:46 -0600

    Initial Commit

---
+++ SwiftAppClimaApiJSON/SwiftAppClimaApiJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAppClimaApiJSONApp.swift
+//  SwiftAppClimaApiJSON
+//
+//  Created by Carlos Delgado on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAppClimaApiJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
